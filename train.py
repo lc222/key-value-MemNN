76125024aff27ef4d7314764b7d6c02b938da4b4
lc222 <254557889@qq.com>
2017-10-11 19:32:26 +0800

    更新参数

--- train.py
+++ train.py
@@ -7,15 +7,15 @@ import datetime
 import os
 
 flags = tf.app.flags
-flags.DEFINE_float("learning_rate", 0.001, "Learning rate for Adam Optimizer.")
+flags.DEFINE_float("learning_rate", 0.005, "Learning rate for Adam Optimizer.")
 #flags.DEFINE_float("epsilon", 1e-8, "Epsilon value for Adam Optimizer.")
-flags.DEFINE_float("max_grad_norm", 5.0, "Clip gradients to this norm.")
+flags.DEFINE_float("max_grad_norm", 10.0, "Clip gradients to this norm.")
 
 flags.DEFINE_integer("batch_size", 32, "Batch size for training.")
-flags.DEFINE_integer("max_slots", 64, "maximum slots in the memory")
+flags.DEFINE_integer("max_slots", 300, "maximum slots in the memory")
 flags.DEFINE_integer("hops", 2, "Number of hops in the Memory Network.")
 flags.DEFINE_integer("epochs", 100, "Number of epochs to train for.")
-flags.DEFINE_integer("embedding_size", 128, "Embedding size for embedding matrices.")
+flags.DEFINE_integer("embedding_size", 200, "Embedding size for embedding matrices.")
 #flags.DEFINE_integer("dropout_memory", 1.0, "keep probability for keeping a memory slot")
 
 flags.DEFINE_string("checkpoint_dir", "checkpoints", "checkpoint directory [checkpoints]")
